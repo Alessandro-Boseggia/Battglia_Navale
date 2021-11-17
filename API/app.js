90a6f4d857bc784d775507a79022a09487df3e98
Akessandro Boseggia <alessandro.boseggia02@gmail.com>
2021-11-17 16:54:31 +0100

    ALTER: API divisa in server.js e app.js

--- API/app.js
+++ API/app.js
@@ -6,26 +6,32 @@ import("dotenv").then((dotenv) => {
     dotenv.config();
 });
 
-const fastify = Fastify();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-fastify.register(autoLoad, {
-    dir: join(__dirname, "plugin"),
-    forceESM: true,
-});
+export default async function app(config = {}) {
+    const fastify = Fastify(config);
 
-fastify.register(autoLoad, {
-    dir: join(__dirname, "src/routers"),
-    forceESM: true,
-});
+    fastify.register(autoLoad, {
+        dir: join(__dirname, "plugin"),
+        forceESM: true,
+    });
 
-fastify.setValidatorCompiler(({ schema, method, url, httpPart }) => {
-    return (data) => schema.validate(data);
-});
+    fastify.register(autoLoad, {
+        dir: join(__dirname, "src/routers"),
+        forceESM: true,
+    });
 
-fastify.listen(3000, async (err, address) => {
-    if (err) return console.error(err);
-    console.log(`Server start on ${address}`);
-});
+    fastify.setValidatorCompiler(({ schema, method, url, httpPart }) => {
+        return (data) => schema.validate(data);
+    });
+
+    fastify.post("/test", (response, reply) => {
+        if (response.body.password === "bose123") {
+            reply.code(200).send();
+        }
+        reply.code(400).send();
+    });
+
+    return fastify;
+}
