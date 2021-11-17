46840947907d96bc3abb52ba451a78e0835a6e0e
Akessandro Boseggia <alessandro.boseggia02@gmail.com>
2021-11-17 16:54:59 +0100

    ADD: aggiunti auth test

---
+++ API/test/auth.test.js
@@ -0,0 +1,72 @@
+beforeAll(async () => {
+    await fastify.mysql.deleteUserByUsername("Ale041");
+});
+
+afterAll(() => {
+    fastify.mysql.closeAllConnection();
+});
+
+test("Insert user", async () => {
+    const response = await fastify.inject({
+        method: "POST",
+        url: "/auth/register",
+        payload: {
+            username: "Ale041",
+            password: "bose123",
+        },
+    });
+
+    expect(response.statusCode).toBe(200);
+});
+
+test("Insert duplicated user", async () => {
+    const response = await fastify.inject({
+        method: "POST",
+        url: "/auth/register",
+        payload: {
+            username: "Ale041",
+            password: "bose123",
+        },
+    });
+
+    expect(response.statusCode).toBe(409);
+});
+
+test("Login user", async () => {
+    const response = await fastify.inject({
+        method: "POST",
+        url: "/auth/login",
+        payload: {
+            username: "Ale041",
+            password: "bose123",
+        },
+    });
+
+    expect(response.statusCode).toBe(200);
+});
+
+test("Login with invalid user", async () => {
+    const response = await fastify.inject({
+        method: "POST",
+        url: "/auth/login",
+        payload: {
+            username: "Pino",
+            password: "bose123",
+        },
+    });
+
+    expect(response.statusCode).toBe(401);
+});
+
+test("Login with invalid password", async () => {
+    const response = await fastify.inject({
+        method: "POST",
+        url: "/auth/login",
+        payload: {
+            username: "Ale041",
+            password: "bosy123",
+        },
+    });
+
+    expect(response.statusCode).toBe(401);
+});
