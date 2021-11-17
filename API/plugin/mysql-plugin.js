1b212ada864485638bc565360e1f4023c341a476
Akessandro Boseggia <alessandro.boseggia02@gmail.com>
2021-11-17 16:58:44 +0100

    ALTER: Aggiunte due funzioni
    - Funzione per chiudere tutte le connessioni a mysql
    - Funzione per eliminare un utente tramite username

--- API/plugin/mysql-plugin.js
+++ API/plugin/mysql-plugin.js
@@ -18,7 +18,24 @@ async function mysql_plugin(fastify, opts) {
         })
         .promise();
 
-    async function getUserByID(id) {}
+    function closeAllConnection() {
+        pool.end();
+    }
+
+    async function deleteUserByUsername(username) {
+        await pool.execute("DELETE FROM Utenti WHERE username = ?", [username]);
+    }
+
+    async function checkDuplicateUsername(username) {
+        const [result] = await pool.execute(
+            "SELECT * FROM Utenti WHERE username = ?",
+            [username]
+        );
+
+        if (result.length >= 1) return true;
+
+        return false;
+    }
 
     async function checkUsernameAndPassword(username, password) {
         const [result] = await pool.execute(
@@ -27,35 +44,33 @@ async function mysql_plugin(fastify, opts) {
         );
 
         if (result.length < 1) throw fastify.httpErrors.unauthorized();
-
-        const passwd = await bcrypt.compare(password, result[0].Password);
+        const passwd = await bcrypt.compare(password, result[0].password);
 
         if (!passwd) throw fastify.httpErrors.unauthorized();
 
         const idUser = result[0].id;
-
         return { idUser };
     }
 
     async function insertUser(username, password) {
-        try {
-            const salt = await bcrypt.genSalt(13);
-
-            password = await bcrypt.hash(password, salt);
-
-            await pool.execute(
-                "INSERT INTO Utenti (username, password) VALUES (?, ?)",
-                [username, password]
-            );
-        } catch (err) {
-            console.log(err);
+        if (await checkDuplicateUsername(username))
             throw fastify.httpErrors.conflict();
-        }
+
+        const salt = await bcrypt.genSalt(13);
+
+        password = await bcrypt.hash(password, salt);
+
+        await pool.execute(
+            "INSERT INTO Utenti (username, password) VALUES (?, ?)",
+            [username, password]
+        );
     }
 
     fastify.decorate("mysql", {
         checkUsernameAndPassword,
         insertUser,
+        closeAllConnection,
+        deleteUserByUsername,
     });
 }
 
