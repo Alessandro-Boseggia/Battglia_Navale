3505775dcfcc935f2c6197e6529af17c57812970
Alessandro Boseggia <alessandro.boseggia02@gmail.com>
2022-01-01 12:37:04 +0100

    ALTER: modifiche alla validazione degli input

--- API/src/routers/auth.route.js
+++ API/src/routers/auth.route.js
@@ -10,14 +10,14 @@ async function authRoute(fastify, opts) {
     fastify.route({
         method: "POST",
         url: "/auth/login",
-        schema: authSchema.loginSchema,
+        schema: { body: authSchema.loginSchema },
         handler: authController.login,
     });
 
     fastify.route({
         method: "POST",
         url: "/auth/register",
-        schema: authSchema.registerSchema,
+        schema: { body: authSchema.registerSchema },
         handler: authController.register,
     });
 
